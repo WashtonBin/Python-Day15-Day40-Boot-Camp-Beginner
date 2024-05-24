6be310b44747ac0a9b04d6c5a7544bfa34d843ca
WashtonBin <jiangbin88424@gmail.com>
2024-05-24 17:53:01 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
 
 #Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+# student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
+# for (index, row) in student_data_frame.iterrows():
     #Access index and row
     #Access row.student or row.score
-    pass
+    # pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# {"A": "Alfa", "B": "Bravo"}
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 word_data_frame = pandas.DataFrame(data)
