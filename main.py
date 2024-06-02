3b4bdd4ab6ecf856bf17082840fa4eb2b4d137ea
WashtonBin <jiangbin88424@gmail.com>
2024-06-01 23:52:27 -0400

    automated sent birthday wishes to people, needs to replace email address and password

---
+++ main.py
@@ -0,0 +1,43 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+my_email = "xxxx@gmail.com"
+password = "xxxxxxxx"
+
+
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    random_letter = random.randint(1,3)
+    file_path = f"letter_templates/letter_{random_letter}.txt"
+    with open(file_path, "r") as file_data:
+        contents = file_data.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("SMTP.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject: Happy Birthday\n\n{contents}")
+
+
+
+
+connection.close()
+
+
+
+
+
+
+
+
